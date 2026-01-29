dfa2e0934a2efc50aef5aeae121886bbb0e9605f
Lena Ivana Ritan <lritan@student.tgm.ac.at>
2026-01-29 11:28:09 +0100

    ACID-Transactions VER. Ritan 3BHWII

--- processor.py
+++ processor.py
@@ -26,21 +26,29 @@ class ShipmentProcessor:
             # STEP 1: Update Inventory
             # This will raise sqlite3.IntegrityError if stock becomes negative
             cursor.execute("UPDATE inventory SET stock_qty = stock_qty - ? WHERE item_name = ?", 
-                           (quantity, item_name))
+                        (quantity, item_name))
             log_callback(">> STEP 1 SUCCESS: Inventory Deducted.")
 
         except sqlite3.IntegrityError as e:
             log_callback(f">> STEP 1 FAILED: {e}") 
-            # Hint: The code doesn't stop here! It continues to Step 2!
-
+            # NEU
+            cursor.execute("SELECT stock_qty FROM inventory WHERE item_name = ?", (item_name,))
+            current_stock = cursor.fetchone()[0] 
+            if current_stock <= 0:
+                log_callback("}\n >> Das Produkt ist ausverkauft!")
+            else:
+                log_callback(f"\n >> Es sind nur {current_stock} {item_name} auf Lager! ")
+            conn.rollback()
+            conn.close()
+            return
         try:
             # STEP 2: Log the Shipment
             cursor.execute("INSERT INTO shipment_log (item_name, qty_moved) VALUES (?, ?)", 
-                           (item_name, quantity))
+                        (item_name, quantity))
             log_callback(">> STEP 2 SUCCESS: Shipment Logged.")
         
         except Exception as e:
-             log_callback(f">> STEP 2 FAILED: {e}")
+            log_callback(f">> STEP 2 FAILED: {e}")
 
         # Final Commit
         conn.commit()
